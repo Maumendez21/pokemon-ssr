d3f9dbd094a5ec54173235ea89a81bb7b4de3ede
Mauro Mendez <maumendezm21@hotmail.com>
2025-01-14 09:56:21 -0600

    first commit

---
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './contact-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone:true
+})
+export default class ContactPageComponent { }
---
+++ src/app/pages/pricing-page/pricing-page.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  templateUrl: './pricing-page.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone:true
+})
+export default class PricingPageComponent { }
